00887e1248a0952b4108cdafbfa9026ced570a3a
Noel Perez <noel999@gmail.com>
2023-01-23 17:32:14 -0400

    Added Chat component with initial styling

---
+++ client/src/components/chat/Chat.jsx
@@ -0,0 +1,19 @@
+import './Chat.scss';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+
+function Chat() {
+  return (
+
+    <div className="chat-container">
+
+      <div className='chat'>
+        <ModeCommentIcon />
+
+      </div>
+      <div className="dot"></div>
+
+    </div>
+  )
+}
+
+export default Chat;
